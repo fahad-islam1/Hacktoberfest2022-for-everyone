2723ae38cd65a6e22ef85d9bf530dc3237b4c518
Fahad islam <78997963+fahad-islam1@users.noreply.github.com>
2022-10-25 18:56:11 +0500

    Create sorting.dart

---
+++ Dart/sorting.dart
@@ -0,0 +1,12 @@
+// Main function
+main() {
+   
+  // Creating List
+  List<int> list = [13, 2, -11, 142, -389, 32, 3032, 0];
+   
+  // Sorting List
+  list.sort();
+   
+  // Printing Sorted List
+  print(list);
+}
